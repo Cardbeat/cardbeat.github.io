4ffe1ce73cc6cf6087bd9dbec4934534216e7df0
Jefferson Oliveira <cardbeatt@gmail.com>
2020-03-20 06:20:43 -0300

    final push

---
+++ precache-manifest.ea05b2b41ccdab3c0e547faf241d6ddf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8dad2f920841b3ebd9947b60f04e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc38453b60b6d5f7fb9a",
+    "url": "/static/js/2.8b77560b.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.8b77560b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1ac78d670a78370451",
+    "url": "/static/js/main.1e3f14eb.chunk.js"
+  },
+  {
+    "revision": "4043f6cc9f7142bf0e30",
+    "url": "/static/js/runtime-main.033fa67d.js"
+  },
+  {
+    "revision": "06925a7b3c15892ebd74ab7ef66d5610",
+    "url": "/static/media/btc.06925a7b.png"
+  },
+  {
+    "revision": "12d7b4cab3b02458fd8469444f8eb95b",
+    "url": "/static/media/draw-dat.12d7b4ca.png"
+  },
+  {
+    "revision": "541ee8a11029d319f288e16a067a4984",
+    "url": "/static/media/pinclone.541ee8a1.png"
+  },
+  {
+    "revision": "175a4f95fb3a8b1f10085217a91df076",
+    "url": "/static/media/voo-nautica.175a4f95.png"
+  }
+]);
