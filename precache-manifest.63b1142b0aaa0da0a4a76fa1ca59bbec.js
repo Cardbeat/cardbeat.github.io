da9767ab8c50abd9c756cf933e93458da5da97fe
Jefferson Oliveira <cardbeatt@gmail.com>
2020-03-20 04:59:59 -0300

    new portfolio

---
+++ precache-manifest.63b1142b0aaa0da0a4a76fa1ca59bbec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0369de5ffb49b9359df251f149b30726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "456e57d40968e74a0df3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b9570797ef22ad74542",
+    "url": "/static/js/2.9329b656.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9329b656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456e57d40968e74a0df3",
+    "url": "/static/js/main.d852f931.chunk.js"
+  },
+  {
+    "revision": "4043f6cc9f7142bf0e30",
+    "url": "/static/js/runtime-main.033fa67d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
